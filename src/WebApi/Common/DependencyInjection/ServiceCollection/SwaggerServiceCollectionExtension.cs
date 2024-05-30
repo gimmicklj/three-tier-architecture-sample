9c4d2a933babc18a06dac63a13c702a36a0946cc
gimmick <908375319@qq.com>
2024-05-30 13:50:52 +0800

    第三次提交

---
+++ src/WebApi/Common/DependencyInjection/ServiceCollection/SwaggerServiceCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace WebApi.Extensions.ServiceCollection;
+
+public static class SwaggerServiceCollectionExtension
+{
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen( c =>
+            c.SwaggerDoc("v1", new OpenApiInfo 
+            { 
+                Title = "Three Tier Architecture In ASP.NET Core 8 Web API", 
+                Version = "v1",
+                Description = "This API provides endpoints for interacting with the Three Tier architecture in ASP.NET Core 8.",
+                Contact = new OpenApiContact
+                {
+                    Name = "Gimmick",
+                    Email = "jielitodo@qq.com",
+                    Url = new Uri("https://github.com/gimmicklj")
+                }
+            })
+        );
+        return services;
+    }
+}
