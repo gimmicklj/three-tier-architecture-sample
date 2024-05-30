f32e1651336b1db7dce1638cba85df6aafce5a48
gimmick <908375319@qq.com>
2024-05-30 14:42:29 +0800

    第6次提交

--- src/DAL/Models/Item.cs
+++ src/DAL/Models/Item.cs
@@ -4,10 +4,8 @@ namespace DAL.Models;
 
 public class Item: Entity<Guid>
 {  
-    [Required]
     public string? Name { get; set; }
     
     
-    [Required]
     public string? Description { get; set; }
 }
